f38bd75253832378a5a2ee5f10e09d7cacb27e49
Yury Kashnitsky <foofest@gmail.com>
2024-08-30 14:15:36 +0200

    Update task.py

--- src/trainer/task.py
+++ src/trainer/task.py
@@ -90,7 +90,6 @@ def train(training_data_dir: str, validation_data_dir: str, output_dir: str) -> 
     os.makedirs(output_dir, exist_ok=True)
     joblib.dump(model, f"{output_dir}/model.joblib")
 
-    
     y_pred = model.predict(X_test)
     log_metrics(y_pred, y_test, output_dir)
 
